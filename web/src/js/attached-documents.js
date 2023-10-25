177d304d2b9070e1dbce21871396cbcdaf7e3e90
Alejandro Suárez <alejandro.suarez@tothomweb.com>
2023-10-25 12:41:23 +0200

    Attached documents changes and notification change in overflow to make it work for both notification variations

--- web/src/js/attached-documents.js
+++ web/src/js/attached-documents.js
@@ -3,17 +3,14 @@ const attachedDocuments = () => {
 
     attachedDocuments.forEach(function(attached){
         const link = attached.getElementsByClassName("doc-link")[0];
-        const a11yText = attached.getElementsByClassName("document-a11y")[0];
 
         link.onclick = () => {
             attached.classList.toggle("active");
 
             if (link.getAttribute("aria-expanded") == "false") {
                 link.setAttribute("aria-expanded", "true");
-                a11yText.innerText = ": Tancar panell descarrega";
             } else {
                 link.setAttribute("aria-expanded", "false");
-                a11yText.innerText = ": Obrir panell descarrega";
             }
         }
     });
